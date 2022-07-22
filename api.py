e1bed6778859bb727d59e6a159ffcf15af0a32f9
John Balzac <crypxtic@gmail.com>
2022-07-22 16:46:19 -0700

    Update api.py

--- api.py
+++ api.py
@@ -7,9 +7,24 @@ url = "<webhook url>"
 data = {}
 result = {}
 
+@app.route("/)
+async def start():
+    data["content"] = "Factorio monitoring started!"
+    data["username"] = "Factorio Bot"
+
+    result = requests.post(url, json=data, headers={"Content-Type": "application/json"})
+    
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print(f"Success!, code {result.status_code}.")
+        return {"status":200}
+
 @app.route("/server-start")
 async def start():
-    data["content"] = "Factorio is server starting..."
+    data["content"] = "Factorio is server starting!"
     data["username"] = "Factorio Bot"
 
     result = requests.post(url, json=data, headers={"Content-Type": "application/json"})
